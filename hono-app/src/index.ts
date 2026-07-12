2ac584e59e12671aaba98251af83f25b267fcc3b
noyonalways <noyonrahman2003@gmail.com>
2026-07-13 00:48:28 +0600

    Add CORS middleware to Hono application

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
+import { corsMiddleware } from "./middlewares/cors.middleware";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+app.use("*", corsMiddleware());
 
 app.get("/message", (c) => {
 
